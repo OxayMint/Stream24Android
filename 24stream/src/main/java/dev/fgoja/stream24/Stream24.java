242a8138664a5176d036c4597a16f3cd468ce826
Farhad Gojazade <farhad.g@bestcomp.net>
2023-07-17 19:01:43 +0400

    fixed issues, specified jdk version

--- 24stream/src/main/java/dev/fgoja/stream24/Stream24.java
+++ 24stream/src/main/java/dev/fgoja/stream24/Stream24.java
@@ -28,20 +28,19 @@ public class Stream24 {
                            Stream24ResultType resultType,
                            Stream24ContentType contentType){
 
-        return String.format("  <!DOCTYPE html>" +
-                "<html lang=\"en\">" +
-                "<head>" +
-                "<meta charset=\"UTF-8\">" +
-                "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1\" />" +
-                "<title>24 Stream</title>" +
-                "</head>" +
-                "<body>" +
-                "<div class=\"wrapper\"></div>" +
-                "<script>(function (w, d, s, o) { var f = d.getElementsByTagName(s)[0]; var j = d.createElement(s); w.TTLStreamReady = new Promise((resolve) => { j.async = true; j.src = 'https://content.24ttl.stream/widget.js'; f.parentNode.insertBefore(j, f); j.onload = function () { w.ttlStream = new TTLStream(o); resolve(w.ttlStream); }; }); })(window, document, 'script', {}); </script>" +
-                "<script>" +
-                "const parameters = { \"brand\": \"%s\", \"productId\": \"%s\", \"retailerDomain\": \"%s\", \"templateType\": \"%s\", \"resultType\": \"%s\", \"contentType\": \"%s\", \"el\": \".wrapper\", \"windowMode\": \"self\" }\n" +
-                "TTLStreamReady.then(() => { ttlStream.findAndInsert(parameters); }); </script>" +
-                "</body>" +
+        return String.format(                "<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1\" />\n" +
+                "    <title>24 Stream</title>\n" +
+                "    <script async=\"\" src=\"https://content.24ttl.stream/widget.js\"></script>\n"+
+                "</head>\n" +
+                "<body>\n" +
+                "    <div class=\"wrapper\"></div>\n" +
+                "    <script>(function (w, d, s, o) { var f = d.getElementsByTagName(s)[0]; var j = d.createElement(s); w.TTLStreamReady = new Promise((resolve) => { j.async = true; j.src = 'https://content.24ttl.stream/widget.js'; f.parentNode.insertBefore(j, f); j.onload = function () { w.ttlStream = new TTLStream(o); resolve(w.ttlStream); }; }); })(window, document, 'script', {}); </script>\n" +
+                "    <script> TTLStreamReady.then(() => { ttlStream.findAndInsert({ \"brand\": \"%s\", \"productId\": \"%s\", \"retailerDomain\": \"%s\", \"templateType\": \"%s\", \"resultType\": \"%s\", \"contentType\": \"%s\", \"el\": \".wrapper\", \"windowMode\": \"self\" }); }); </script>\n" +
+                "</body>\n" +
                 "</html>", brand, productId, retailerDomain, templateType, _getContentType(contentType), _getResultType(resultType));
     }
     static String _getContentType(Stream24ContentType contentType) {
